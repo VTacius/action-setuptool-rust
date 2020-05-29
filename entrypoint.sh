c27185774c97a67eee8bd221f016b88a5c2fc62f
Alexander Ortiz <vtacius@gmail.com>
2020-05-29 15:03:06 -0600

    ¡Nueva versión! Nuestro problema ya no es la construcción propiamente dicha, sino detalles sobre la implementación en github

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,9 @@ python3 setup.py $comando
 id="uid"
 html_url=$(ls dist/)
 upload_url=$(ls *)
+fichero=$(find dist/ -mindepth 1)
 
 echo ::set-output name=${CARGO_BIN}
 echo ::set-output name=html_url::$html_url
 echo ::set-output name=upload_url::$upload_url
+echo ::set-output name=fichero::$fichero
