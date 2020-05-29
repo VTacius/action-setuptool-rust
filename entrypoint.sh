f4b37999449fd1a3b0f6cb8ecb441d6e1eb862e8
Alexander Ortiz <vtacius@gmail.com>
2020-05-29 14:00:23 -0600

    Sexto intento. Creo que el último

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -x
 set -e -x
 
-comando=$1
+CARGO_BIN=$(dirname $(find / -wholename '*cargo/bin/cargo'))
+export PATH="${CARGO_BIN}:${PATH}"
 
+rustup default nightly
+
+comando=$1
 python3 setup.py $comando
 
 id="uid"
 html_url=$(ls -l)
 upload_url=$(ls /)
 
-echo ::set-output name=${contenido}
+echo ::set-output name=${CARGO_BIN}
 echo ::set-output name=html_url::$html_url
 echo ::set-output name=upload_url::$upload_url
