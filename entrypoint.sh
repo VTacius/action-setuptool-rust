7bbdd56727528ffced789efb2a97d2770f34f680
Alexander Ortiz <vtacius@gmail.com>
2020-06-05 08:48:37 -0600

    ¡Nueva versión! Creo que con esto tengo para darle a upload/artifact todo lo necesario

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,8 @@ rustup default nightly
 comando=$1
 python3 setup.py $comando
 
-id="uid"
-html_url=$(ls dist/)
-upload_url=$(ls *)
-fichero=$(find dist/ -mindepth 1)
+ruta_paquete=$(find dist/ -mindepth 1)
+nombre_paquete=$(basename $ruta_paquete)
 
-echo ::set-output name=${CARGO_BIN}
-echo ::set-output name=html_url::$html_url
-echo ::set-output name=upload_url::$upload_url
-echo ::set-output name=fichero::$fichero
+echo ::set-output name=ruta_paquete::$ruta_paquete
+echo ::set-output name=nombre_paquete::$nombre_paquete
